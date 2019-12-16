a97223cfdadd3baf49f291bd8d228575df760aaa
Dancassini <papeari@hotmail.fr>
2019-12-16 23:19:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>"+"<li><a href='index.html'> Home </a></li>"+"<li><a href='projets.html'> Projets </a></li>"+
+ "<li><a href='about.html'> About </a></li>"+
+"</ul>"; 
+
+document.getElementById("foot01").innerHTML =  new Date().getFullYear() + "<p> Premier exercice</p>";
